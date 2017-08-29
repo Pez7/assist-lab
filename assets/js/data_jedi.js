6b5b76355e3d73ebf3ad05740ee86fd2437a0950
Maria Paz <maria.pazrojasc@gmail.com>
2017-08-29 11:56:42 -0300

    archivos json

---
+++ assets/js/data_jedi.js
@@ -0,0 +1,112 @@
+var jedi = [
+    {
+        "nombre": "Blanca Perez",
+        "edad": "25",
+        "foto": "img/foto001.png",
+        "jedi_id": "jm001",
+        "username": "Blanca",
+        "password": "jedimaster",
+        "correo": "blanca@labotatoria.la",
+        "telefono": "56995272010",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Belén Recabal",
+        "edad": "23",
+        "foto": "img/foto002.png",
+        "id_jedi": "jm002",
+        "username": "Belen",
+        "password": "jedimaster",
+        "correo": "belen@labotatoria.la",
+        "telefono": "56995272050",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Nadia Tapia",
+        "edad": "28",
+        "foto": "img/foto003.png",
+        "id_jedi": "jm003",
+        "username": "Nadia",
+        "password": "jedimaster",
+        "correo": "nadia@labotatoria.la",
+        "telefono": "56992632030",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Marcia Millanao",
+        "edad": "21",
+        "foto": "img/foto004.png",
+        "id_jedi": "jm004",
+        "username": "Marcia",
+        "password": "jedimaster",
+        "correo": "marcia@labotatoria.la",
+        "telefono": "56976288012",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Alexandra Neira",
+        "edad": "19",
+        "foto": "img/foto005.png",
+        "id_jedi": "jm005",
+        "username": "Alexandra",
+        "password": "jedimaster",
+        "correo": "alexandra@labotatoria.la",
+        "telefono": "56975666062",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Allison Gúzman",
+        "edad": "27",
+        "foto": "img/foto006.png",
+        "id_jedi": "jm006",
+        "username": "Allison",
+        "password": "jedimaster",
+        "correo": "allison@labotatoria.la",
+        "telefono": "56999962123",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Francisca Rojas",
+        "edad": "25",
+        "foto": "img/foto007.png",
+        "id_jedi": "jm007",
+        "username": "Francisca",
+        "password": "jedimaster",
+        "correo": "francisca@labotatoria.la",
+        "telefono": "56978772776",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Viviana Segura",
+        "edad": "21",
+        "foto": "img/foto008.png",
+        "id_jedi": "jm008",
+        "username": "Viviana",
+        "password": "jedimaster",
+        "correo": "viviana@labotatoria.la",
+        "telefono": "56961091345",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Patricia Moreno",
+        "edad": "26",
+        "foto": "img/foto009.png",
+        "id_jedi": "jm009",
+        "username": "Patricia",
+        "password": "jedimaster",
+        "correo": "patricia@labotatoria.la",
+        "telefono": "56977118127",
+        "cargo": "Teacher"
+    },
+    {
+        "nombre": "Maite Fuentes",
+        "edad": "20",
+        "foto": "img/foto010.png",
+        "id_jedi": "jm010",
+        "username": "Maite",
+        "password": "jedimaster",
+        "correo": "maite@labotatoria.la",
+        "telefono": "56985154008",
+        "cargo": "Teacher"
+    }
+]
---
+++ assets/js/data_sprint.js
@@ -0,0 +1,66 @@
+var sprint = [
+	{
+		"id": "sp01",
+		"nombre_sprint": "Becoming a Front End Engineer",
+		"objetivo": "Comprender los fundamentos de que componen Javascript",
+		"puntos_tecnicos": "1800",
+		"puntos_hse": "1000",
+		"fecha_inicio": "08-08-2017",
+		"fecha_termino": "23-08-2017" 
+	},
+	
+		"id": "sp02",
+		"nombre_sprint": "Prototyping",
+		"objetivo": "frameworks CSS",
+		"puntos_tecnicos": "1800",
+		"puntos_hse": "1000",
+		"fecha_inicio": "24-08-2017",
+		"fecha_termino": "08-09-2017" 
+	},
+	{
+		"id": "sp03",
+		"nombre_sprint": "Herramientas de productividad",
+		"objetivo": "Utilizar Gulp",
+		"puntos_tecnicos": "1800",
+		"puntos_hse": "1000",
+		"fecha_inicio": "11-09-2017",
+		"fecha_termino": "26-09-2017" 
+	},
+		{
+		"id": "sp04",
+		"nombre_sprint": "Ajax y API Calls",
+		"objetivo": "Realizar llamados de API",
+		"puntos_tecnicos": "1800",
+		"puntos_hse": "1000",
+		"fecha_inicio": "27-09-2017",
+		"fecha_termino": "13-10-2017" 
+	},
+		{
+		"id": "sp05",
+		"nombre_sprint": "Eventos con jQuery",
+		"objetivo": "Aprender a utilizar la librería de Javascript",
+		"puntos_tecnicos": "1800",
+		"puntos_hse": "1000",
+		"fecha_inicio": "16-10-2017",
+		"fecha_termino": "27-10-2017" 
+	},
+		{
+		"id": "sp06",
+		"nombre_sprint": "SASS",
+		"objetivo": "Utilizar los pre-procesadores de CSS",
+		"puntos_tecnicos": "1800",
+		"puntos_hse": "1000", 
+		"fecha_inicio": "30-10-2017",
+		"fecha_termino" "17-11-2017"
+	},
+		{
+		"id": "sp07",
+		"nombre_sprint": "Node y NPM",
+		"objetivo": "Herramientas de productividad",
+		"puntos_tecnicos": "1800",
+		"puntos_hse": "1000",
+		"fecha_inicio": "20-11-2017",
+		"fecha_termino": "06-12-2017" 
+	},
+	
+]
